486a1968665388e9bebca3293589c66825001990
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-03-11 15:28:57 +0100

    plugin first

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: tagger
+# about: simple tagging support for discourse
+# version: 0.1
+# authors: Benjamin Kampmann
+
+
+register_asset "javascripts/discourse/templates/composer_tagging.js.handlebars"
+register_asset "javascripts/composer_tagging.js"
+
