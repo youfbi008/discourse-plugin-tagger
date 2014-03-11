07554d665f32834b9be5c9534dd778ee95dd724a
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-03-11 15:58:19 +0100

    only show if taggable entry

--- assets/javascripts/composer_tagging.js
+++ assets/javascripts/composer_tagging.js
@@ -2,25 +2,39 @@
 Discourse.ComposerTagsView = Discourse.View.extend({
 	templateName: "composer_tagging",
 
+	model: Em.computed.alias('controller.model'),
+
 	insertTagsView: function() {
 		this._insertElementLater(function() {
-			var target = this._parentView.$("#draft-status");
-			if (target.length === 0) return;
-      		this.$().insertAfter(target);
+			var target = this._parentView.$();
+			console.log(target);
+      		target.append(this.$());
+      		console.log(this.get("model"));
       	}.bind(this));
 	}
 });
 
+Discourse.Composer.reopen({
+
+	can_be_tagged: function(){
+		// only creation of new topic and editing of first post are valid for us
+		console.log("asked");
+		return this.get("creatingTopic") || (
+			this.get("editingPost") && this.get("editingFirstPost"))
+	}.property("creatingTopic", "editingPost", "editingFirstPost")
+
+});
+
 
 Discourse.ComposerView.reopen({
-	childDidInsertElement: function() {
+	insertTagsView: function() {
 		this._super();
 		if (this.get("tagsview")) return;
 
 		var view = this.createChildView(Discourse.ComposerTagsView,
 			 {controller: this.get("controller")});
 		view.insertTagsView();
-		this.set("tagsview", view)
 		console.log(view);
-	}
+		this.set("tagsview", view)
+	}.on("didInsertElement")
 });
