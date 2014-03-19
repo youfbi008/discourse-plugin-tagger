177c7813a560700f4a505ff7c95bac3a72905f08
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-03-19 15:28:56 +0100

    rerender items professionally

--- assets/javascripts/topic_tags.js
+++ assets/javascripts/topic_tags.js
@@ -1,22 +1,49 @@
 Discourse.TagsView = Discourse.View.extend({
 	templateName: "topic_tags",
+	attributeBindings: ["model"],
 
 	model: Em.computed.alias('controller.model'),
+
 	insertTagsView: function() {
-		this._insertElementLater(function() {
-			var target = this._parentView.$("h1");
-      		this.$().insertAfter(target);
-      	}.bind(this));
+		var view = this;
+		if (view.state === "inDOM") return;
+
+		Ember.run.schedule('afterRender', this, function(){
+			var target = view._parentView.$("h1").parent();
+			if (target.length) {
+				if (view.state === "preRender") view.createElement();
+				target.append(view.$());
+			}
+      	});
 	}
 });
+
 Discourse.TopicView.reopen({
-	addTags: function(){
-		this._super();
+	insertTagsView: function() {
 		if (this.get("tagsview")) return;
 
 		var view = this.createChildView(Discourse.TagsView,
-			 {controller: this.get("controller")});
+                        {controller: this.get("controller")});
 		view.insertTagsView();
 		this.set("tagsview", view)
-	}.on("didInsertElement")
+	}.on("didInsertElement"),
+
+	ensureTags: function(){
+		if (!this.get("tagsview")) return;
+		this.get("tagsview").insertTagsView();
+	}.observes('topic.tags', 'topic.loaded'),
+
+	removeTags: function(){
+		if (this.get("tagsview")){
+			this.get("tagsview").destroy();
+			this.set("tagsview", null);
+		}
+	}.on("willClearRender"),
+
+	killTags: function(){
+		if (this.get("tagsview")){
+			this.get("tagsview").destroy();
+			this.set("tagsview", null);
+		}
+	}.on("willDestroyElement")
 });
