f2ffa90e76ec2cfecafb3aa2e5ed9a629654baee
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-04-17 19:04:05 +0200

    page specific tags

--- assets/javascripts/sidebar_tags.js
+++ assets/javascripts/sidebar_tags.js
@@ -5,14 +5,49 @@
 
     var SidebarTagCloudView = Discourse.View.extend({
         templateName: "sidebar_tag_cloud",
-        refresh: function(){
-            console.log("IN");
-            if (this.get("tags")) return;
-            Discourse.ajax("/tagger/tags/cloud").then(function(resp){
-                console.log(resp);
-                this.set("tags", resp.cloud);
-            }.bind(this))
-        }.on("didInsertElement")
+        init: function () {
+            this._super();
+            this.__cache = {};
+        },
+        // refresh: function(){
+        //     if (this.get("tags")) return;
+        //     Discourse.ajax("/tagger/tags/cloud").then(function(resp){
+        //         console.log(resp);
+        //         this.set("tags", resp.cloud);
+        //     }.bind(this))
+        // }.on("didInsertElement"),
+
+        urlChanged: function(){
+            this.set("loading", true);
+            var name = this.get("currentControllerName"),
+                router = Discourse.URL.get("router").router,
+                url = "/tagger/tags/cloud";
+            if (name.indexOf("topic") === 0) { // one of the topic views
+                url = "/tagger/tags/cloud?topic_id=" + router.currentParams.id;
+            } else if (name === "discovery.category"){
+                // inside a specific category we only want related to that cat
+                if (router.currentParams.slug){
+                    url = "/tagger/tags/cloud?category_slug=" + router.currentParams.slug;
+                }
+            } else if (name === "tagged.tag"){
+                // tag-page, load similar tags:
+                var tag_name = this.get("url").split("/")[1];
+                url = "/tagger/tags/cloud?tag=" + tag_name;
+            }
+
+            console.log(url);
+
+            if (url in this.__cache){
+                this.set("tags", this.__cache[url]);
+                this.set("loading", false);
+            } else {
+                Discourse.ajax(url).then(function(resp){
+                    this.__cache[url] = resp.cloud;
+                    this.set("tags", resp.cloud);
+                    this.set("loading", false);
+                }.bind(this));
+            }
+        }
     });
 
     Discourse.SidebarView.reopen({
