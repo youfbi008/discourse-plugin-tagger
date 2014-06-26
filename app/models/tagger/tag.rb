1fa48743329be749981b1c041d2e24dab29a394a
Piioo <Piotr.Szal@googlemail.com>
2014-06-26 14:04:21 +0200

    cache tags for cloud in redis

--- app/models/tagger/tag.rb
+++ app/models/tagger/tag.rb
@@ -3,15 +3,15 @@ module Tagger
   	has_and_belongs_to_many :topic, :class_name => "Topic"
 
     def self.cloud_for_topic(topic_id)
-      tag_ids = nil#$redis.get("tag_cloud_for_topic_#{topic_id}")
+      tags = $redis.get("tag_cloud_for_topic_#{topic_id}")
 
-      return Tagger::Tag.where(id: tag_ids.split(',')) unless tag_ids.nil?
+      return Marshal.load(tags) unless tags.nil?
 
       tags = Tagger::Tag.select("tagger_tags.title, COUNT(tagger_tags_topics.topic_id) as count")
                    .group("tagger_tags.id")
                    .joins(:topic)
                    .where("tagger_tags_topics.topic_id IN (SELECT tagger_tags_topics.topic_id FROM tagger_tags_topics WHERE tagger_tags_topics.tag_id in (SELECT tagger_tags_topics.tag_id FROM tagger_tags_topics WHERE tagger_tags_topics.topic_id = ? )) ", topic_id)
-      #$redis.setex("tag_cloud_for_topic_#{topic_id}", 15.minutes, tags.to_json)
+      $redis.setex("tag_cloud_for_topic_#{topic_id}", 15.minutes, Marshal.dump(tags))
 
       tags
     end
