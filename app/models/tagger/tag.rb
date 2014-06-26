cc375090ee51396bf29fc81ecff70bf89dc65ef1
Piioo <Piotr.Szal@googlemail.com>
2014-06-26 13:55:11 +0200

    working tagger plugin

--- app/models/tagger/tag.rb
+++ app/models/tagger/tag.rb
@@ -3,7 +3,7 @@ module Tagger
   	has_and_belongs_to_many :topic, :class_name => "Topic"
 
     def self.cloud_for_topic(topic_id)
-      tag_ids = $redis.get("tag_cloud_for_topic_#{topic_id}")
+      tag_ids = nil#$redis.get("tag_cloud_for_topic_#{topic_id}")
 
       return Tagger::Tag.where(id: tag_ids.split(',')) unless tag_ids.nil?
 
@@ -11,7 +11,7 @@ module Tagger
                    .group("tagger_tags.id")
                    .joins(:topic)
                    .where("tagger_tags_topics.topic_id IN (SELECT tagger_tags_topics.topic_id FROM tagger_tags_topics WHERE tagger_tags_topics.tag_id in (SELECT tagger_tags_topics.tag_id FROM tagger_tags_topics WHERE tagger_tags_topics.topic_id = ? )) ", topic_id)
-      $redis.setex("tag_cloud_for_topic_#{topic_id}", 15.minutes, tags.pluck(:id).join(','))
+      #$redis.setex("tag_cloud_for_topic_#{topic_id}", 15.minutes, tags.to_json)
 
       tags
     end
