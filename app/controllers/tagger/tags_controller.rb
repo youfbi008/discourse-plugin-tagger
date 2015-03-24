e42a90775917b8bfe447a04b312ae2d992115830
Marc Schopper <marc@schopp3r.de>
2015-03-24 15:48:22 +0100

    Fix issue for missing tags

--- app/controllers/tagger/tags_controller.rb
+++ app/controllers/tagger/tags_controller.rb
@@ -74,15 +74,13 @@ module Tagger
     def get_topics_per_tag
       params.require(:tag)
       @tag = Tag.find_by("title = ?", params[:tag])
-      @title = @tag.title
       return render json: false if @tag.blank?
 
       @list = TopicList.new(:tag, current_user, topics_query)
       
       respond_to do |format|
         format.html do 
-          @headline = @tag.title
-          @description_meta = @tag.title
+          @title = @description_meta = @headline = @tag.title
           if @tag.listable?
             @robots_meta_index, @robots_meta_follow = 'index', 'follow'
           else
